33042d6d7aebb95be971704df603e8609674455a
mlacosta <marianoacosta.003@gmail.com>
2020-07-15 20:35:36 -0300

    ver 1.4.0 change background

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -15,7 +15,7 @@ import {Block,blockFactory} from './blocks.js';
 import {Game, levelUpflag, setLevelUpflag, displayScore, setdisplayScore,scoreCounter,setScoreCounter} from './game.js';
 import {inputHandler} from './utils.js';
 
-let version = '1.3.0';
+let version = '1.4.0';
 
 let playfield = document.getElementById('playfield');
 let next = document.getElementById('next');
