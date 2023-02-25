f4543d349259411e615ca5ff16464aa90362c4e8
mlacosta <marianoacosta.003@gmail.com>
2023-02-25 18:21:09 -0300

    create constant file

--- src/main/blocks.js
+++ src/main/blocks.js
@@ -1,16 +1,4 @@
-const BLOCK_COLORS = [
-  "#fcba03",
-  "#fc3103",
-  "#f8fc03",
-  "#adfc03",
-  "#03fc03",
-  "#03ebfc",
-  "#0367fc",
-  "#6203fc",
-  "#ce03fc",
-  "#fc039d",
-  "#800040",
-];
+
 
 export class Block {
   constructor(params, position, color) {
---
+++ src/main/constants.js
@@ -0,0 +1,13 @@
+export const BLOCK_COLORS = [
+  "#fcba03",
+  "#fc3103",
+  "#f8fc03",
+  "#adfc03",
+  "#03fc03",
+  "#03ebfc",
+  "#0367fc",
+  "#6203fc",
+  "#ce03fc",
+  "#fc039d",
+  "#800040",
+];
